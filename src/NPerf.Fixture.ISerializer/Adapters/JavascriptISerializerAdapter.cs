0888e0838429755954dad9ddc9d85dbc2493ba0e
ovidiomanteiga@gmail.com <ovidiomanteiga@gmail.com>
2013-10-13 01:48:31 +0200

    Changing DataContractJsonSerializer by JavaScriptSerializer due to problems with the former (it needs attributes on the classes in order to work well).

---
+++ src/NPerf.Fixture.ISerializer/Adapters/JavascriptISerializerAdapter.cs
@@ -0,0 +1,19 @@
+﻿namespace NPerf.Fixture.ISerializer.Adapters
+{
+    using System.Web.Script.Serialization;
+
+    public class JavascriptISerializerAdapter<T> : ISerializer<T>
+    {
+        private JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+        public string Serialize(T theObject)
+        {
+            return serializer.Serialize(theObject);
+        }
+
+        public T Deserialize(string serialized)
+        {
+            return serializer.Deserialize<T>(serialized);
+        }
+    }
+}
